0339079e717de37d2e9bf0b15d7b2ff8504ea28a
Kakhagigashvili <kaxagigashvili@gmail.com>
2026-07-29 00:13:40 +0400

    custom user model and authentication

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,8 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd-party apps
+    "crispy_forms",
+    "crispy_bootstrap5",
     # local apps
     'pages',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -117,4 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIR = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / 'static']
+AUTH_USER_MODEL = "accounts.CustomUser"
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
